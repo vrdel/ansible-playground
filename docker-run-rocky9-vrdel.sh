e9557322ac36bf43499affcc2ed2cbe06010c342
Daniel Vrcic <daniel.vrcic@gmail.com>
2024-07-01 17:44:18 +0200

    fix docker run scripts

--- docker-run-rocky9-vrdel.sh
+++ docker-run-rocky9-vrdel.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-test -z $1 && TAG="latest" || TAG="$1"
-
 docker run \
---privileged \
+--cap-add=SYS_ADMIN \
+--device /dev/snd \
+--device /dev/dri \
 -e "DISPLAY=unix$DISPLAY" \
 --log-driver json-file \
 --log-opt max-size=10m \
 -v /dev/log:/dev/log \
 -v /etc/localtime:/etc/localtime \
+-v "${HOME}":/mnt/ \
+-v "${HOME}":/home/user/ \
+-v "${HOME}"/.ssh:/home/user/.ssh/ \
 -v /tmp/.X11-unix:/tmp/.X11-unix \
--v $HOME/.zsh_history:/home/user/.zsh_history \
--v $HOME:/mnt/ \
--v $HOME/.ssh:/home/user/.ssh/ \
---tmpfs=/run --tmpfs=/tmp \
--v /sys/fs/cgroup:/sys/fs/cgroup \
--h docker-rocky9 \
+-v "${HOME}"/.zsh_history:/home/user/.zsh_history \
+-h rocky1 \
 --net host \
---name rocky9-vrdel \
+--name rocky1 \
 --rm -ti \
-ipanema:5000/rocky9-vrdel:$TAG
-
+vrdel-ansible-rocky1 /bin/zsh
--- docker-run-ubuntu-vrdel.sh
+++ docker-run-ubuntu-vrdel.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-test -z $1 && TAG="latest" || TAG="$1"
-
-# docker run -d \
 docker run \
 --cap-add=SYS_ADMIN \
 --device /dev/snd \
@@ -12,13 +9,13 @@ docker run \
 --log-opt max-size=10m \
 -v /dev/log:/dev/log \
 -v /etc/localtime:/etc/localtime \
--v $HOME:/mnt/ \
--v $HOME:/home/user/ \
--v $HOME/.ssh:/home/user/.ssh/ \
+-v "${HOME}":/mnt/ \
+-v "${HOME}":/home/user/ \
+-v "${HOME}"/.ssh:/home/user/.ssh/ \
 -v /tmp/.X11-unix:/tmp/.X11-unix \
--v $HOME/.zsh_history:/home/user/.zsh_history \
+-v "${HOME}"/.zsh_history:/home/user/.zsh_history \
 -h ubuntu-c \
 --net host \
 --name ubuntu-c \
 --rm -ti \
-vrdel-ansible-ubuntu-c:$1 /bin/zsh
+vrdel-ansible-ubuntu-c /bin/zsh
