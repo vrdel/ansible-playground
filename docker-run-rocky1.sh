e898a7348e37bbf9d930b7805f743bd89f4631da
Daniel Vrcic <daniel.vrcic@gmail.com>
2024-07-17 21:08:00 +0200

    pass ssh-agent into containers and refine chezmoi script to automatically identify github.com

--- docker-run-rocky1.sh
+++ docker-run-rocky1.sh
@@ -5,12 +5,13 @@ docker run \
 --device /dev/snd \
 --device /dev/dri \
 -e "DISPLAY=unix$DISPLAY" \
+-e "SSH_AUTH_SOCK=${SSH_AUTH_SOCK}" \
+--mount type=bind,src="${SSH_AUTH_SOCK}",target="${SSH_AUTH_SOCK}" \
 --log-driver json-file \
 --log-opt max-size=10m \
 -v /dev/log:/dev/log \
 -v /etc/localtime:/etc/localtime \
 -v "${HOME}":/mnt/ \
--v "${HOME}"/.ssh:/home/user/.ssh/ \
 -v /tmp/.X11-unix:/tmp/.X11-unix \
 -h rocky1 \
 --net host \
--- docker-run-ubuntuc.sh
+++ docker-run-ubuntuc.sh
@@ -7,15 +7,18 @@ docker run \
 --device /dev/snd \
 --device /dev/dri \
 -e "DISPLAY=unix$DISPLAY" \
+-e "SSH_AUTH_SOCK=${SSH_AUTH_SOCK}" \
+--mount type=bind,src="${SSH_AUTH_SOCK}",target="${SSH_AUTH_SOCK}" \
 --log-driver json-file \
 --log-opt max-size=10m \
 -v /dev/log:/dev/log \
 -v /etc/localtime:/etc/localtime \
 -v "${HOME}":/mnt/ \
--v "${HOME}"/.ssh:/home/user/.ssh/ \
 -v /tmp/.X11-unix:/tmp/.X11-unix \
 -h ubuntuc \
 --net host \
 --name ubuntuc \
 --rm -ti \
 vrdel-ansible-ubuntuc /bin/zsh
+
+# -v "${HOME}"/.ssh:/home/user/.ssh/ \
